aadf7ae8b980dee4ae736a89d84f6d60e357557a
Simon Schitz <schitzsimon@gmail.com>
2025-04-11 08:43:44 +0200

    add test

--- src/test/java/org/example/oldman_and_sea/entity/QuestTest.java
+++ src/test/java/org/example/oldman_and_sea/entity/QuestTest.java
@@ -1,11 +1,17 @@
 package org.example.oldman_and_sea.entity;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QuestTest {
     private Quest quest;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        quest = new Quest();
+    }
 
     @Test
     public void testCongratulations(){
